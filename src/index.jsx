e7549fd6f4278a04a3b4206f4554584540f02a6d
Saul Pagador <saulpizarro76@gmail.com>
2022-12-23 10:48:31 +0100

    ended match removal

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import "./index.scss";
 
 const status = ['Not Started', '1st half', 'Halftime', '2nd half', 'Ended']
 
-var footballWordCupScoreBoard = [
+const footballWordCupScoreBoard = [
   ["Time", "Home Team", "Away Team", "Score", "Status"],
   ["18:00", "Mexico", "Canada", '', status[0]],
   ["19:00", "Spain", "Brazil", '', status[0]],
@@ -16,17 +16,10 @@ var footballWordCupScoreBoard = [
 
 export class Board extends React.Component {
 
-  constructor(props){
-    super(props);
-    this.state = {
-      boardData: props.boardData ?? []
-    }
-  } 
-
   render() {
     const rowResult = [];
   
-    this.state.boardData.forEach((data, i) => {
+    this.props.boardData?.forEach((data, i) => {
       rowResult.push(
         <Row key={i} labelList={data} isHeader={i===0}/>
       );
@@ -73,7 +66,7 @@ class FootballBoard extends React.Component {
   startLoop() {
     let time = 0;
     const footballInterval = setInterval(() => {
-      let newBoard = matchLogic(time++, this.props.footballBoard);
+      let newBoard = matchLogic(time++, this.state.footballBoard);
       let newState = summaryLogic(newBoard, this.state.summaryBoard);
       this.setState({
         footballBoard: newState[0],
@@ -81,7 +74,7 @@ class FootballBoard extends React.Component {
       })
     }, 1000);
 
-    setTimeout(() => clearInterval(footballInterval), 5000);
+    setTimeout(() => clearInterval(footballInterval), 6000);
   }
   
 
@@ -93,10 +86,18 @@ class FootballBoard extends React.Component {
       </React.Fragment>
     ) : '';
 
-    return (
+    let mainBoard = (this.state.footballBoard?.length > 1) ? (
       <React.Fragment>
         <h1>{'Matches'}</h1>
         <Board boardData={this.state.footballBoard}/>
+      </React.Fragment>
+    ) : '';
+
+    
+
+    return (
+      <React.Fragment>
+        {mainBoard}
         {summary}
       </React.Fragment>
     )
@@ -113,36 +114,36 @@ if(elementRef) {
 function matchLogic(loop, board) {
   switch(loop) {
     case 0: 
-      board[1] = updateMatch(board[1], "18:00", '', 0);
-      board[2] = updateMatch(board[2], "19:00", '', 0);
-      board[3] = updateMatch(board[3], "HalfTime", '0-0', 2);
-      board[4] = updateMatch(board[4], "10'", '0-2', 1);
-      board[5] = updateMatch(board[5], "50'", '3-0', 3);
+      board[1] = updateMatch(footballWordCupScoreBoard[1], "18:00", '', 0);
+      board[2] = updateMatch(footballWordCupScoreBoard[2], "19:00", '', 0);
+      board[3] = updateMatch(footballWordCupScoreBoard[3], "HalfTime", '0-0', 2);
+      board[4] = updateMatch(footballWordCupScoreBoard[4], "10'", '0-2', 1);
+      board[5] = updateMatch(footballWordCupScoreBoard[5], "50'", '3-0', 3);
       break;
     case 1: 
-      board[1] = updateMatch(board[1], "10'", '0-1', 1);
-      board[2] = updateMatch(board[2], "19:00", '', 0);
-      board[3] = updateMatch(board[3], "50'", '2-2', 3);
-      board[4] = updateMatch(board[4], "HalfTime", '3-4', 2);
-      board[5] = updateMatch(board[5], "15:00", '3-1', 4);
+      board[1] = updateMatch(footballWordCupScoreBoard[1], "10'", '0-1', 1);
+      board[2] = updateMatch(footballWordCupScoreBoard[2], "19:00", '', 0);
+      board[3] = updateMatch(footballWordCupScoreBoard[3], "50'", '2-2', 3);
+      board[4] = updateMatch(footballWordCupScoreBoard[4], "HalfTime", '3-4', 2);
+      board[5] = updateMatch(footballWordCupScoreBoard[5], "15:00", '3-1', 4);
       break;
     case 2: 
-      board[1] = updateMatch(board[1], "HalfTime", '0-3', 2);
-      board[2] = updateMatch(board[2], "10'", '2-0', 1);
-      board[3] = updateMatch(board[3], "16:00", '2-2', 4);
-      board[4] = updateMatch(board[4], "50'", '4-4', 3);
+      board[1] = updateMatch(footballWordCupScoreBoard[1], "HalfTime", '0-3', 2);
+      board[2] = updateMatch(footballWordCupScoreBoard[2], "10'", '2-0', 1);
+      board[3] = updateMatch(footballWordCupScoreBoard[3], "16:00", '2-2', 4);
+      board[4] = updateMatch(footballWordCupScoreBoard[4], "50'", '4-4', 3);
       break;
     case 3: 
-      board[1] = updateMatch(board[1], "50'", '0-4', 3);
-      board[2] = updateMatch(board[2], "HalfTime", '8-0', 2);
-      board[4] = updateMatch(board[4], "17:00", '6-6', 4);
+      board[1] = updateMatch(footballWordCupScoreBoard[1], "50'", '0-4', 3);
+      board[2] = updateMatch(footballWordCupScoreBoard[2], "HalfTime", '8-0', 2);
+      board[3] = updateMatch(footballWordCupScoreBoard[4], "17:00", '6-6', 4);
       break;
     case 4: 
-      board[1] = updateMatch(board[1], "18:00", '0-5', 4);
-      board[2] = updateMatch(board[2], "50'", '9-0', 3);
+      board[1] = updateMatch(footballWordCupScoreBoard[1], "18:00", '0-5', 4);
+      board[2] = updateMatch(footballWordCupScoreBoard[2], "50'", '9-0', 3);
       break;
     default:
-      board[2] = updateMatch(board[2], "19:00", '10-2', 4);
+      board[1] = updateMatch(footballWordCupScoreBoard[2], "19:00", '10-2', 4);
       break;
   }
 
@@ -157,10 +158,10 @@ function updateMatch(match, time, score, statusIndex) {
 }
 
 function summaryLogic(board, summaryBoard) {
-  board = board.filter((match) => {
+  let newBoard = board.filter((match) => {
     
     return match[4] !== status[4];
   });
-  console.log(board);
-  return [board, summaryBoard];
+  console.log(newBoard);
+  return [newBoard, summaryBoard];
 }
